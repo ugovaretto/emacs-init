747e8221e3fb1a8b6fd575ca5f0233c5f19f987f
Ugo Varetto <ugovaretto@gmail.com>
2020-03-16 19:04:50 +0800

    updated - magit, helm, evil-tabs

--- init.el
+++ init.el
@@ -1,40 +1,44 @@
 (require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (when no-ssl (warn "\
-Your version of Emacs does not support SSL connections,
-which is unsafe because it allows man-in-the-middle attacks.
-There are two things you can do about this warning:
-1. Install an Emacs version that does support SSL and be safe.
-2. Remove this warning from your init file so you won't see it again."))
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-  ;; and `package-pinned-packages`. Most users will not need or want to do this.
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  )
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                     ("marmalade" . "http://marmalade-repo.org/packages/")
+                     ("melpa" . "http://melpa.org/packages/")))
+
+
+(setq package-enable-at-startup nil)
 (package-initialize)
-(package-refresh-contents)
+
+(unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+      (package-install 'use-package))
+
+(eval-when-compile
+    (require 'use-package))
+
+(use-package evil :ensure t)
+(use-package helm :ensure t)
+(use-package magit :ensure t)
+(use-package evil-tabs :ensure t)
+
+(require 'evil)
+(evil-mode t)
+(global-evil-tabs-mode t)
+(powerline-evil-center-color-theme)
+(require 'helm)
+(helm-mode t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (wombat)))
- '(package-selected-packages (quote (powerline-evil powerline evil))))
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
+ '(custom-enabled-themes (quote (tsdh-dark)))
+ '(package-selected-packages (quote (helm use-package powerline-evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-;; Enable Evil
-(require 'evil)
-(evil-mode 1)
-
-;; Enable Powerline
-(require 'powerline)
-(powerline-center-evil-theme)
-
-;; Enable Powerline-evil
-(require 'powerline-evil)
