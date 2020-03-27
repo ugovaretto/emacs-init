9ea001856977426001da3b4c81f3d3d92c34ce98
Ugo Varetto <ugovaretto@gmail.com>
2020-03-27 23:56:23 +0800

    ...

--- init.el
+++ init.el
@@ -14,15 +14,18 @@
 (eval-when-compile
     (require 'use-package))
 
-(use-package evil :ensure t)
+;;(use-package evil :ensure t)
 (use-package helm :ensure t)
 (use-package magit :ensure t)
-(use-package evil-tabs :ensure t)
+(use-package powerline :ensure t)
+(use-package wrap-region :ensure t)
+(use-package markdown-mode :ensure t)
+;;(use-package evil-tabs :ensure t)
 
-(require 'evil)
-(evil-mode t)
-(global-evil-tabs-mode t)
-(powerline-evil-center-color-theme)
+;;(require 'evil)
+;;(evil-mode t)
+;;(global-evil-tabs-mode t)
+;;(powerline-center-color-theme)
 (require 'helm)
 (helm-mode t)
 (custom-set-variables
@@ -30,15 +33,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(custom-enabled-themes (quote (tsdh-dark)))
- '(package-selected-packages (quote (helm use-package powerline-evil))))
+ '(custom-enabled-themes (quote (manoj-dark)))
+ '(package-selected-packages
+   (quote
+    (wrap-region markdown-mode use-package sublime-themes spacemacs-theme powerline-evil magit helm evil-tabs))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:background nil)))))
